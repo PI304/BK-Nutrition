e865091a0d53ce8621fe4f6e45fd66948776046e
HyunseokLEE <unducklee@gmail.com>
2023-02-02 17:05:32 +0900

    Initial commit from Create BK-Nutrition
    * chore: install prettier
    
    * chore: apply prettier & eslint
    
    * chore: install axios & tsconfig fix
    
    * chore: make instance & api
    
    * chore: make type instance & api
    
    * chore: install styled-components & apply styled-components
    
    * chore: apply reset & global styles
    
    * chore: default page delete
    
    * chore: apply .env & .gitignore
    
    * chore: apply custom head
    
    * chore: apply custom head
    
    * chore: colors & fonts constant
    
    * chore: apply layout
    
    * chore: public img del & plus
    
    * chore: render page del & plus
    
    * chore: components page plus
    
    * chore: custom head fix
    
    * color & font & global css plus
    
    * chore: reference page plus

--- constants/seo.ts
+++ constants/seo.ts
@@ -4,6 +4,10 @@ export const Seo = {
     intro: 'Intro',
     member: 'Member',
     notice: 'Notice',
+<<<<<<< HEAD
+=======
+    reference: 'Reference',
+>>>>>>> c2a9e7b (Initial commit from Create BK-Nutrition)
     //notFound: 'Not Found',
   },
 } as const;
