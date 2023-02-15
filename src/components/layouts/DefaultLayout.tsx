213c4b268af2e59f4a7aa9fd9346c6eda7d2cf2b
HyunseokLEE <unducklee@gmail.com>
2023-02-16 02:30:57 +0900

    feat: header & footer

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,12 +1,20 @@
 import styled from 'styled-components';
+import { Footer, Header } from '../Adds';
 
 export const DefaultLayout = ({ children }: ChildrenType) => {
-  return <S.Layout>{children}</S.Layout>;
+  return (
+    <S.Layout>
+      <Header />
+      {children}
+      <Footer />
+    </S.Layout>
+  );
 };
 
 namespace S {
   export const Layout = styled.div`
     width: 100vw;
     height: 100vh;
+    position: relative;
   `;
 }
