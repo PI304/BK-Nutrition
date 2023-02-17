23a75b7f1732d1ef778a307b65d588d27efa87c8
HyunseokLEE <unducklee@gmail.com>
2023-02-18 00:38:28 +0900

    feat: tab color

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -6,4 +6,5 @@ export const Colors = {
   yonseishadow: '#003777; #000000; 20%',
   header: 'rgba(0, 0, 0, 0.1)',
   contact: 'rgba(18, 18, 18, 0.55)',
+  shadow: 'rgba(57, 57, 57, 0.6)',
 } as const;
