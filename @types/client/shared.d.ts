68d35c2422eda2f74af33bf2fdf8a9093fd481e2
HyunseokLEE <unducklee@gmail.com>
2023-02-21 00:59:45 +0900

    feat: pagination

--- @types/client/shared.d.ts
+++ @types/client/shared.d.ts
@@ -6,3 +6,12 @@ type TitleProps = {
   title: string;
   address: string;
 };
+
+type PageButtonProps = {
+  currentPage: number;
+  totalPosts: number | undefined;
+  size: number | undefined;
+  onChangePage: (page: number) => void;
+};
+
+type NumberButtonProps = IsCurrentType & {};
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,1 +1,4 @@
 export * from './BackgroundHeader';
+export * from './Members';
+export * from './Pagination';
+export * from './PageButton';
