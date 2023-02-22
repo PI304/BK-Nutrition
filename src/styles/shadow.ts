4115f93dacb6660110c99d189b831361016b3e45
HyunseokLEE <unducklee@gmail.com>
2023-02-23 02:20:54 +0900

    feat: styled

--- src/styles/shadow.ts
+++ src/styles/shadow.ts
@@ -1,4 +1,6 @@
 export const BoxShadows = {
   smooth: '0px 2px 10px rgba(0, 0, 0, 0.1);',
   nomal: '0px 4px 4px rgba(0, 0, 0, 0.25);',
+  download:
+    '0px 4px 4px rgba(0, 0, 0, 0.25), inset 4px 4px 4px rgba(255, 255, 255, 0.25), inset -4px -4px 4px rgba(0, 0, 0, 0.25);',
 } as const;
