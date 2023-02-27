df59e220ccf660fc871ce46a87c292832e509680
HyunseokLEE <unducklee@gmail.com>
2023-02-28 02:32:48 +0900

    feat: styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import { ResetCSS } from './reset';
+import { Colors } from './colors';
 
 export const GlobalStyle = createGlobalStyle`
   ${ResetCSS}
@@ -11,11 +12,13 @@ export const GlobalStyle = createGlobalStyle`
   html, body {
     font-family: 'Inter', sans-serif;
     font-style: normal;
+    
   }
 
   * {
     font-size: 1.6rem;
     font-weight: 400;
     box-sizing: border-box;
+    color: ${Colors.gray900};
   }
 `;
